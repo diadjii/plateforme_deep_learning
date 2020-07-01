e7571053178ac6ed52cdcc6c2659fe44e5e999b3
niijaay <diadjindiaye24@gmail.com>
2020-07-01 14:50:36 +0000

    rename views

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/accueil')
+def accueil():
     return render_template('gestion_datas.html.j2')
 
 if __name__ == '__main__':
