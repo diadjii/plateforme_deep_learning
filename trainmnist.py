47d5625ada68ab447412ddee0fd3c7a42f329ddf
ibson14 <ib.thiamass@gmail.com>
2020-07-24 12:53:05 +0000

    visualisation des metrics MNIST Loss et accuracy

--- trainmnist.py
+++ trainmnist.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import mpld3
 
 def mnist_model():
-    (train_images, train_labels), (val_images, val_labels) = mnist.load_data()
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
     network = models.Sequential()
     network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
@@ -18,11 +18,11 @@ def mnist_model():
 
     train_images = train_images.reshape((60000, 28 * 28))
     train_images = train_images.astype('float32') / 255
-    val_images = val_images.reshape((10000, 28 * 28))
-    val_images = val_images.astype('float32') / 255
+    test_images = test_images.reshape((10000, 28 * 28))
+    test_images = test_images.astype('float32') / 255
 
     train_labels = to_categorical(train_labels)
-    val_labels = to_categorical(val_labels)
+    test_labels = to_categorical(test_labels)
 
     hist = network.fit(train_images, train_labels, epochs=5, batch_size=128, validation_split=.1)
         
@@ -32,6 +32,15 @@ def mnist_model():
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Val'], loc='lower right')
-    plt.savefig('static/images/mnist.png', dpi=100)
+    plt.savefig('static/images/mnistaccuracy.png', dpi=100)
+    plt.clf()
+
+    plt.plot(hist.history['loss'])
+    plt.plot(hist.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Val'], loc='lower right')
+    plt.savefig('static/images/mnistloss.png', dpi=100)
     
     
